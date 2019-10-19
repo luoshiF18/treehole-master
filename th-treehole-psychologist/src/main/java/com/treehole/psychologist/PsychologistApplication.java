1d8115c3c4985718237516bf0ecd11539a8d4d76
Helayxy <1562823234@qq.com>
2019-10-19 19:55:43 +0800

    初始化心理咨询师微服务

---
+++ th-treehole-psychologist/src/main/java/com/treehole/psychologist/PsychologistApplication.java
@@ -0,0 +1,11 @@
+package com.treehole.psychologist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologistApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PsychologistApplication.class, args);
+    }
+}
