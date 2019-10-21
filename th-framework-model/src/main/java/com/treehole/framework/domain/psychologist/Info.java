4ae90dcc383d4e7abcca69612c1e7e6ede107dd9
Helayxy <1562823234@qq.com>
2019-10-21 18:57:57 +0800

    重命名文件名

--- th-framework-model/src/main/java/com/treehole/framework/domain/psychologist/Info.java
+++ th-framework-model/src/main/java/com/treehole/framework/domain/psychologist/Info.java
@@ -1,6 +1,5 @@
 package com.treehole.framework.domain.psychologist;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
 import tk.mybatis.mapper.annotation.KeySql;
@@ -9,6 +8,7 @@ import tk.mybatis.mapper.code.Style;
 
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 /**
  * 心理咨询师基本信息
@@ -17,26 +17,20 @@ import javax.persistence.Table;
 @ToString
 @Table(name = "psychologist_info")
 @NameStyle(Style.normal)
-public class info {
+public class Info implements Serializable {
 
     @Id
     @KeySql(useGeneratedKeys = true)
-    @ApiModelProperty("心理咨询师id")
     private String id;
 
-    @ApiModelProperty("心理咨询师姓名")
     private String name;
 
-    @ApiModelProperty("心理咨询师年龄")
     private Integer age;
 
-    @ApiModelProperty("心理咨询师性别 0：男 1：女")
     private Integer gender;
 
-    @ApiModelProperty("心理咨询师所在地区")
     private String region;
 
-    @ApiModelProperty("心理咨询师专业资质（等级）")
     private String grade;
 
 }
