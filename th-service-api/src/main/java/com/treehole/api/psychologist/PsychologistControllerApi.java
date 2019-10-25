2a166317c5cdd998acc2afbed9dff4e47a0252fe
Helayxy <1562823234@qq.com>
2019-10-25 16:56:30 +0800

    添加根据id查询心理咨询师信息接口

--- th-service-api/src/main/java/com/treehole/api/psychologist/PsychologistControllerApi.java
+++ th-service-api/src/main/java/com/treehole/api/psychologist/PsychologistControllerApi.java
@@ -1,5 +1,6 @@
 package com.treehole.api.psychologist;
 
+import com.treehole.framework.domain.psychologist.Psychologist;
 import com.treehole.framework.model.response.QueryResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,7 +12,10 @@ import io.swagger.annotations.ApiOperation;
 @Api(value = "心理咨询师信息管理", description = "对心理咨询师信息进行增、删、改、查")
 public interface PsychologistControllerApi {
 
-    @ApiOperation("分页查询心理咨询师信息")
+    @ApiOperation("分页查询所有心理咨询师信息")
     QueryResponseResult findAllPsychologist(Integer page, Integer size);
+
+    @ApiOperation("根据id查询心理咨询师信息")
+    Psychologist findPsychologistById(String id);
 
 }
