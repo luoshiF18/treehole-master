b3ade315b25f84691d175fbc318864e7565178a1
Helayxy <1562823234@qq.com>
2019-11-16 20:27:30 +0800

    添加详情信息相关mapper

---
+++ th-treehole-psychologist/src/main/java/com/treehole/psychologist/dao/DetailMapper.java
@@ -0,0 +1,11 @@
+package com.treehole.psychologist.dao;
+
+import com.treehole.framework.domain.psychologist.Detail;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author Helay
+ * @date 2019/11/16 10:12
+ */
+public interface DetailMapper extends Mapper<Detail>{
+}
