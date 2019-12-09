618ad843a52b9a32d0858590a129c629e89cc746
shanhuijie <1747734858@qq.com>
2019-12-09 19:36:47 +0800

    cards 更新2

--- th-treehole-member/src/main/java/com/treehole/member/MemberApplication.java
+++ th-treehole-member/src/main/java/com/treehole/member/MemberApplication.java
@@ -3,6 +3,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -13,6 +15,9 @@ import tk.mybatis.spring.annotation.MapperScan;
  * @date 2019/10/16 15:10
  * @description: 会员管理
  */
+
+@EnableFeignClients //feignClient
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.treehole.member.mapper")
 @EntityScan("com.treehole.framework.domain.member")//扫描实体类
@@ -22,6 +27,6 @@ import tk.mybatis.spring.annotation.MapperScan;
 @EnableSwagger2
 public class MemberApplication  {
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class);
+        SpringApplication.run(MemberApplication.class,args);
     }
 }
