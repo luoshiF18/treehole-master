5ae67ebbee3730b293140bc09fb4a13156e61b55
OcheanRzq <1023357383@qq.com>
2019-12-20 14:42:01 +0800

    archives打开注释

--- th-treehole-archives/src/main/java/com/treehole/archives/service/PersonArchivesService.java
+++ th-treehole-archives/src/main/java/com/treehole/archives/service/PersonArchivesService.java
@@ -5,18 +5,26 @@ import com.github.pagehelper.PageHelper;
 import com.treehole.archives.client.ScaleSelectClient;
 import com.treehole.archives.client.UserVoClient;
 import com.treehole.archives.dao.ArchivesResultMapper;
+import com.treehole.framework.domain.archives.ext.ArchivesExt;
 import com.treehole.framework.domain.archives.ext.ArchivesList;
+import com.treehole.framework.domain.archives.ext.ResultTiny;
 import com.treehole.framework.domain.archives.response.ArchivesCode;
 import com.treehole.framework.domain.archives.response.ArchivesCountResult;
 import com.treehole.framework.domain.archives.resquest.ArchivesListRequest;
+import com.treehole.framework.domain.evaluation.response.DetailResult;
+import com.treehole.framework.domain.evaluation.vo.ScaleDetailVO2;
+import com.treehole.framework.domain.member.Vo.UserVo;
 import com.treehole.framework.domain.member.resquest.UserListRequest;
 import com.treehole.framework.exception.ExceptionCast;
 import com.treehole.framework.model.response.CommonCode;
 import com.treehole.framework.model.response.QueryResponseResult;
 import com.treehole.framework.model.response.QueryResult;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author 任志强
@@ -75,17 +83,17 @@ public class PersonArchivesService {
      * @param userId
      * @return
      */
-  /*  private UserVo findUserById(String userId){
+    private UserVo findUserById(String userId){
         UserVo userVoByUserId = userVoClient.getUserVoByUserId(userId);
         return userVoByUserId;  //上层接口已做了空值判定
     }
-*/
+
     /**
      * 根据用户id和量表名查询个人详细档案记录
      * @param archivesListRequest
      * @return
      */
-  /*  public ArchivesExt findArchivesExt(ArchivesListRequest archivesListRequest) {
+    public ArchivesExt findArchivesExt(ArchivesListRequest archivesListRequest) {
         ArchivesExt archivesExt = new ArchivesExt();
         //设置量表名称
         archivesExt.setScaleName(archivesListRequest.getScaleName());
@@ -98,7 +106,7 @@ public class PersonArchivesService {
         //设置答题次数
         archivesExt.setScaleCount(resultTinies.size());
         //根据id获取用户信息
-      *//*  UserVo userVo = findUserById(archivesListRequest.getUserId());*//*
+        UserVo userVo = findUserById(archivesListRequest.getUserId());
         if (userVo == null){
             ExceptionCast.cast(CommonCode.INVALID_PARAM);
         }
@@ -116,7 +124,8 @@ public class PersonArchivesService {
         archivesExt.setTopicBackground(scaleDetail.getTopicBackground());
         return archivesExt;
     }
-*/
+
+
     /**
      * 查询所有用户档案
      * @param page
